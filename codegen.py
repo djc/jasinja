040212ad84782ca2242ecd3b6e500c91b869c41a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2011-05-13 15:13:40 +0200

    Get the most simple if statement working.

--- codegen.py
+++ codegen.py
@@ -8,39 +8,63 @@ class JSCodeGen(CodeGenerator):
 		self.writeline('var template = {')
 		self.indent()
 		self.writeline('')
+		self.writeline('"render": function(ctx) {')
+		self.indent()
+		self.writeline('var _buf = [];')
 		
 		frame = Frame(EvalContext(self.environment, self.name))
+		frame.buffer = '_buf'
+		
 		for n in node.body:
 			self.visit(n, frame)
 		
+		self.writeline('return _buf.join("");')
+		self.outdent()
+		self.writeline('}')
+		self.outdent()
 		self.writeline('')
 		self.outdent()
 		self.writeline('};')
 	
 	def visit_Output(self, node, frame):
-		
-		self.writeline('"output": function(ctx) {')
-		self.indent()
-		
-		self.writeline('return [')
-		self.indent()
-		
 		for n in node.nodes:
+			self.newline()
+			self.write('_buf.push(')
+			self.visit(n, frame)
+			self.write(');')
+	
+	def visit_Name(self, node, frame):
+		self.write('ctx["%s"]' % node.name)
+	
+	def visit_TemplateData(self, node, frame):
+		val = node.as_const(frame.eval_ctx)
+		if isinstance(val, unicode):
+			val = val.encode('utf-8')
+		self.write(repr(val))
+	
+	def visit_If(self, node, frame):
+		
+		self.newline()
+		self.write('if (')
+		self.visit(node.test, frame)
+		self.write(') {')
+		
+		self.indent()
+		for n in node.body:
 			self.visit(n, frame)
 		
 		self.outdent()
-		self.writeline('].join("");')
-		
-		self.outdent()
 		self.writeline('}')
-	
-	def visit_Name(self, node, frame):
-		self.writeline('ctx["%s"]' % node.name)
 
 def compile(env, src):
 	return Parser(env, src, 'test', 'test.html').parse()
 
 def generate(env, tmpl):
 	gen = JSCodeGen(env, 'blah', 'index.html', None, False)
+	gen.visit(tmpl)
+	return gen.stream.getvalue()
+
+def pygen(env, tmpl):
+	gen = CodeGenerator(env, 'blah', 'index.html', None, False)
 	gen.visit(tmpl)
 	return gen.stream.getvalue()
--- test.py
+++ test.py
@@ -1,19 +1,35 @@
-import codegen, jinja2, spidermonkey
+import codegen, jinja2, spidermonkey, sys
 import simplejson as json
 
-def jstest(src, data):
-	env = jinja2.Environment()
+def jstest(env, src, data):
 	run = spidermonkey.Runtime()
 	ctx = run.new_context()
 	js = codegen.generate(env, codegen.compile(env, src))
 	jsobj = json.dumps(data)
-	code = js + '\ntemplate.output(%s);' % jsobj
+	code = js + '\ntemplate.render(%s);' % jsobj
 	return ctx.execute(code)
 
-def pytest(src, data):
-	return jinja2.Template(src, 'test').render(data)
+def pytest(env, src, data):
+	tmpl = env.from_string(src)
+	return tmpl.render(data)
 
-src = '{{ test }}'
-data = {'test': 'crap'}
-print repr(jstest(src, data))
-print repr(pytest(src, data))
+WORKS = [
+	('{{ test }}', {'test': 'crap'}),
+	('{% if a %}x{% endif %}', {'a': True}),
+]
+
+# next:
+# - if/elif/else/endif
+# - assignment + cond-expr
+# - for-loop
+# - object access
+# - filters
+
+src, data = WORKS[int(sys.argv[1])]
+env = jinja2.Environment()
+ast = codegen.compile(env, src)
+print ast
+print codegen.generate(env, ast)
+
+print 'js:', repr(jstest(env, src, data))
+print 'py:', repr(pytest(env, src, data))
