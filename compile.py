08fb7633ff59cedf4924e314033ef47fb6904617
Dirkjan Ochtman <dirkjan@ochtman.nl>
2011-06-03 14:05:02 +0200

    Compile all templates if none are specified.

--- compile.py
+++ compile.py
@@ -2,6 +2,7 @@ import codegen, jinja2, sys
 
 def compile(path, templates):
 	env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
+	templates = templates if templates else env.loader.list_templates()
 	print codegen.generate(env, templates)
 
 if __name__ == '__main__':
